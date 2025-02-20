152358345431be34a24680d4a55ae6d14301dde7
CharuNethra12 <barishcharu@gmail.com>
2025-02-20 22:14:29 +0530

    Login and Account setup (Personalization)

--- lib/screen/login/on_boarding_screen.dart
+++ lib/screen/login/on_boarding_screen.dart
@@ -1,8 +1,8 @@
 import 'package:fitness4all/common/color_extensions.dart';
 import 'package:fitness4all/common_widgets/round_button.dart';
 import 'package:fitness4all/screen/home/settings/settings_screen.dart';
+import 'package:fitness4all/screen/login/sign_up_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({super.key});
@@ -146,7 +146,7 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
                     onPressed: () {
                       if (selectPage >= 3) {
                         //Next Screen
-                          context.push(const SettingScreen());
+                          context.push(const SignUpScreen());
                       } else {
                         selectPage = selectPage + 1;
                         controller.animateToPage(
